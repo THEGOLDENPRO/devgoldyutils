66c53c4424848b24f0292ce8a0a9178fe152d9ad
THEGOLDENPRO <jassim7256@gmail.com>
2022-06-06 03:28:23 +0100

    first commit

---
+++ src/devgoldyutils/console.py
@@ -0,0 +1,47 @@
+import random
+
+class ConsoleColours:
+    """Class that contants methods for colouring the text printed to the console."""
+
+    def RANDOM(self, text:str):
+        random_num = random.randint(1, 4)
+
+        if random_num == 1: return self.GREEN(text)
+        if random_num == 2: return self.BLUE(text)
+        if random_num == 3: return self.CLAY(text)
+        if random_num == 4: return self.PURPLE(text)
+
+    def PICK_COLOUR(self, colour_name:str, text:str):
+        if not colour_name == None:
+            if colour_name.lower() in ["green"]:
+                return self.GREEN(text)
+
+            if colour_name.lower() in ["blue"]:
+                return self.BLUE(text)
+
+            if colour_name.lower() in ["clay"]:
+                return self.CLAY(text)
+
+            if colour_name.lower() in ["purple"]:
+                return self.PURPLE(text)
+        else:
+            return self.RANDOM(text)
+    
+    def GREEN(self, text:str):
+        return "\u001b[32m" + str(text) + "\u001b[0m"
+
+    def BLUE(self, text:str):
+        return "\u001b[36m" + str(text) + "\u001b[0m"
+
+    def CLAY(self, text:str):
+        return "\u001b[38;5;51m" + str(text) + "\u001b[0m"
+
+    def PURPLE(self, text:str):
+        return "\u001b[38;5;200m" + str(text) + "\u001b[0m"
+
+    def RED(self, text:str):
+        return "\u001b[31m" + str(text) + "\u001b[0m"
+
+class Console(ConsoleColours):
+    def __init__(self):
+        pass
---
+++ src/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'Operating System :: POSIX :: Linux',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.8',
+  'Programming Language :: Python :: 3.9'
+]
+
+setup(
+  name='devgoldyutils',
+  version="1.0",
+  description='My own utils libary I use throught all my python projects.', 
+  long_description=open('README.txt').read(), 
+  url='', 
+  project_urls={"Bug Tracker": ""}, 
+  author='Dev Goldy', 
+  author_email='goldy@novauniverse.net', 
+  license='MIT', 
+  classifiers=classifiers, 
+  keywords=["Goldy Utils", "Dev Goldy Utils", "devgoldyutils"], 
+  packages=find_packages(), 
+  include_package_data=True,
+  install_requires=open('requirements.txt').read(),
+  python_requires=">=3.8"
+)
