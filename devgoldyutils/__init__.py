ed0c9ea1f84adf850265c1443264e0c3619a1268
Dev Goldy <jassim7256@gmail.com>
2023-03-10 13:29:06 +0000

    forgot import logging stuff to __init__

--- devgoldyutils/__init__.py
+++ devgoldyutils/__init__.py
@@ -3,3 +3,4 @@ from .console import Console
 from .colours import Colours
 from .dataclasses import DictDataclass
 from .errors import DevGoldyUtilsError
+from .logging import add_custom_handler, LoggerAdapter
