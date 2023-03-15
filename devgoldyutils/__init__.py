33f6688352a631faff73326dfa5046db5094a333
Dev Goldy <jassim7256@gmail.com>
2023-03-15 16:13:56 +0000

    forgot to add it to __init__.py again 💀

--- devgoldyutils/__init__.py
+++ devgoldyutils/__init__.py
@@ -4,3 +4,4 @@ from .colours import Colours
 from .dataclasses import DictDataclass
 from .errors import DevGoldyUtilsError
 from .logging import add_custom_handler, LoggerAdapter
+from .file_configs import JSONConfig
