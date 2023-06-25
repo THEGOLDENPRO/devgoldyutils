0b67cf21a60dba8f1512ea069f8777c11f1340e9
THEGOLDENPRO <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-06-25 15:47:56 +0100

    made pretty printer an optional dependency.
    pprint() is now only available with pip install devgoldyutils[pprint]

--- devgoldyutils/__init__.py
+++ devgoldyutils/__init__.py
@@ -1,3 +1,5 @@
+import importlib
+
 from .console import Console
 
 from .colours import Colours
@@ -12,6 +14,9 @@ from . import better_get
 # For those Americans. Imagine not using English UK.
 Colors = Colours
 
-# Utils from other libraries
-from prettyprinter import cpprint as pprint, install_extras
-install_extras(include=["dataclasses"])
+# Optional utils from other libraries.
+# -------------------------------------
+# Install devgoldyutils with 'pprint' optional dependency to get this like so --> pip install devgoldyutils[pprint]
+if importlib.find_loader("prettyprinter") is not None: 
+    from prettyprinter import cpprint as pprint, install_extras
+    install_extras(include=["dataclasses"])
